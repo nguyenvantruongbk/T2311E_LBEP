bb141de9b157c08db675cd5663532becfdd968de
nguyenvantruongbk <153369920+nguyenvantruongbk@users.noreply.github.com>
2023-12-09 20:33:00 +0700

    Add files via upload

---
+++ var.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+	int X; // khai bao bien X, kieu du lieu dau cach ten bien
+	printf("x = %d\n",X);
+	X = 10;
+	X = 20;
+	float pi = 3.1459;
+	printf("X = 20");
+	X = 30;
+	printf("X = 30");
+//	printf("x = %d",X," x %d",X);
+// int  %d; float %f; double %lf ; char %c  cac ngon ngu bac cao no se tu dong chuyen doi
+// muon xuong dong \n
+	
+	
+	
+}
+
