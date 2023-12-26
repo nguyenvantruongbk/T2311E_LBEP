6fd37f4d18813c9835288b7c6c715400973f6428
nguyenvantruongbk <153369920+nguyenvantruongbk@users.noreply.github.com>
2023-12-26 21:14:22 +0700

    Add files via upload

---
+++ Lab_Slot12/khoang_X_X.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, i, j, min, max;
+    printf("Nhap so phan tu cua mang: ");
+    scanf("%d", &n);
+    int a[n];
+    printf("Nhap cac phan tu cua mang: ");
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    min = a[0];
+    max = a[0];
+    for (i = 1; i < n; i++)
+    {
+        if (a[i] > max)
+            max = a[i];
+        if (a[i] < min)
+            min = a[i];
+    }
+    printf("Doan [-%d, %d] tren truc so nguyen chua tat ca cac gia tri trong mang.\n", max, max);
+    return 0;
+}
