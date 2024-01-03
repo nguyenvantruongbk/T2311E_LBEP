5280a4ec156d962a1d3bac2cebc47fc816cd0445
nguyenvantruongbk <153369920+nguyenvantruongbk@users.noreply.github.com>
2024-01-03 21:50:39 +0700

    Add files via upload

---
+++ SLOT_9_Mang_LAP/trung_binh_Cong_So_Le_Vichi_Chan.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("nhap so phan tu cua mang: ");
+    scanf("%d", &n);
+    int arr[n],count;
+    int sum=0;
+    count=0;
+    for(int i = 0; i < n; ++i) {
+        printf("Nhap phan tu thu %d: ", i+1);
+        scanf("%d", &arr[i]);
+        if(arr[i]%2>0&&(i+1)%2==0){
+        	sum+=arr[i];
+        	count++;
+		}
+    } 
+    //printf("%d %d",sum,count);
+    float avg = sum / count;
+    printf("trung binh cong cac so le trong mang la: %.2f", avg);
+    return 0;
+}
